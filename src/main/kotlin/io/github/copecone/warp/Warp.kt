6e5b961911821cd9c3b3c23d2c3ede4c5adadcd7
노모 <no.moment.prv@gmail.com>
2023-10-20 21:07:35 +0900

    코드 갈아 엎기

---
+++ src/main/kotlin/io/github/copecone/warp/Warp.kt
@@ -0,0 +1,10 @@
+package io.github.copecone.warp
+
+import net.fabricmc.api.ModInitializer
+
+@Suppress("unused")
+object Warp: ModInitializer {
+    const val MOD_ID = "sample"
+
+    override fun onInitialize() {}
+}
